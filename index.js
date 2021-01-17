57f8bd5aa424c19ed3a28ac62298585b43460b6b
Francisco Magana <franciscomagana.contact@gmail.com>
2021-01-17 00:06:48 -0600

    hide the memu in the window

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function initWindow(){
     mainWindow = new BrowserWindow({
         height: 360,
         width: 480,
+        autoHideMenuBar: true
     });
     
     mainWindow.loadFile(path.join(__dirname, 'views/index.html'));
