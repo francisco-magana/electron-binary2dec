d068782f2e32bc3bd00223295215150c724cedc6
Francisco Magana <franciscomagana.contact@gmail.com>
2021-01-17 00:10:47 -0600

    input allows only  1 or 0

--- src/binary2dec.js
+++ src/binary2dec.js
@@ -2,6 +2,12 @@ let binaryValue;
 const btnConvert = document.getElementById('convert');
 const binaryInput = document.getElementById('binary');
 
+binaryInput.addEventListener('keydown', (e) => {
+	if (e.keyCode != 48 && e.keyCode != 49 && e.keyCode != 8) {
+		e.preventDefault();
+	}
+});
+
 btnConvert.addEventListener('click', () => {
 	binaryValue = binaryInput.value;
 	let decimalValue = parseInt(binaryValue, 2);
