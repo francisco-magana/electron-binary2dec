9a83b152a057ea4f1c73d6c66f06720249c7f319
Francisco Magana <franciscomagana.contact@gmail.com>
2021-01-16 23:33:50 -0600

    load the html file into the mainwindow

---
+++ index.js
@@ -0,0 +1,15 @@
+const { BrowserWindow, app } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function initWindow(){
+
+    mainWindow = new BrowserWindow({
+        height: 480,
+        width:360,
+    });
+    
+    mainWindow.loadFile(path.join(__dirname, 'views/index.html'));
+
+};
